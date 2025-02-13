72214ed2870c912ece151f9b46c6bf0eb87d9585
justajay224 <ajaymaulana2224@gmail.com>
2025-02-13 17:35:41 +0700

    commit 2

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 const express = require('express');
 const expressEjsLayouts = require('express-ejs-layouts');
+const morgan = require('morgan')
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
+
+//third-party middleware
 app.use(expressEjsLayouts);
+app.use(morgan('dev'));
+
+//built-in middleware
+app.use(express.static('public'));
+
+//application  level middleware
+app.use((req, res, next) => {
+    console.log('hai')
+    next()
+})
 
 app.get('/', (req, res) => {
     const mahasiswa = [
@@ -17,8 +30,8 @@ app.get('/', (req, res) => {
             email: 'agung@gmail.com'
         },
         {
-            nama: 'ayya',
-            email: 'ayya@gmail.com'
+            nama: 'joko',
+            email: 'joko@gmail.com'
         },
     ]
     res.render('index', {
@@ -45,7 +58,7 @@ app.get('/produk/:id', (req, res) => {
     res.send(`Product ID : ${req.params.id} <br> Category ID : ${req.query.category}`);
 })
 
-app.use('/', (req, res) => {
+app.use((req, res) => {
     res.status(404);
     res.send('404');
 })
